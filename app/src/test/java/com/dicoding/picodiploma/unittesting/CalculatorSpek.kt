094240a5a26cba2a2e2c0abe489243931c8ff4fb
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-12 02:37:54 +0700

    Move SampleCalculator

--- app/src/test/java/com/dicoding/picodiploma/unittesting/CalculatorSpek.kt
+++ app/src/test/java/com/dicoding/picodiploma/unittesting/CalculatorSpek.kt
@@ -28,13 +28,3 @@ class CalculatorSpek : Spek({
         }
     }
 })
-
-class SampleCalculator {
-    fun sum(x: Int, y: Int): Int {
-        return x + y
-    }
-
-    fun subtract(x: Int, y: Int): Int {
-        return x - y
-    }
-}
