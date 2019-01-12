a32cd89002914bcf3b77ab87fc4193b3572d4ad5
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-12 09:34:16 +0700

    Update SampleCalculator.kt

--- app/src/main/java/com/dicoding/picodiploma/unittesting/SampleCalculator.kt
+++ app/src/main/java/com/dicoding/picodiploma/unittesting/SampleCalculator.kt
@@ -1,11 +1,6 @@
 package com.dicoding.picodiploma.unittesting
 
 class SampleCalculator {
-    fun sum(x: Int, y: Int): Int {
-        return x + y
-    }
-
-    fun subtract(x: Int, y: Int): Int {
-        return x - y
-    }
+    fun sum(x: Int, y: Int): Int = x.plus(y)
+    fun subtract(x: Int, y: Int) = x.minus(y)
 }
